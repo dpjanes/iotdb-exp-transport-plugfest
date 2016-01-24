b1b8c6394dcda031a3bc8d7f971876223b893dc1
David Janes <davidjanes@davidjanes.com>
2016-01-23 19:26:35 -0500

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# iotdb-exp-transport-plugfest
-Experimental PlugFest Transport
+# iotdb-transport-prototype
+IOTDB / Home☆Star Transport for Plugfest
+
+<img src="https://raw.githubusercontent.com/dpjanes/iotdb-homestar/master/docs/HomeStar.png" align="right" />
+
+This is an **advanced** topic. 
+[Documentation](https://homestar.io/about/transporters).
+
---
+++ samples/bad_list.js
@@ -0,0 +1,22 @@
+/*
+ *  bad_list.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2016-01-23
+ *
+ *  Deal with data that does not exist
+ *  Expect to see just 'null'
+ */
+
+var Transport = require('../PlugfestTransport').PlugfestTransport;
+
+var transport = new Transport({
+});
+transport.list(function(ld) {
+    if (!ld) {
+        break;
+    }
+
+    console.log("+", ld.id);
+});
