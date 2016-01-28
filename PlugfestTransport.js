2ec9e511142b0ebbf0609695e122c7ada53aa9be
David Janes <davidjanes@davidjanes.com>
2016-01-28 13:38:33 -0500

    update -> push

--- PlugfestTransport.js
+++ PlugfestTransport.js
@@ -219,7 +219,7 @@ PlugfestTransport.prototype.get = function (paramd, callback) {
 /**
  *  See {iotdb_transport.Transport#Transport} for documentation.
  */
-PlugfestTransport.prototype.update = function (paramd, callback) {
+PlugfestTransport.prototype.put = function (paramd, callback) {
     var self = this;
 
     self._validate_update(paramd, callback);
